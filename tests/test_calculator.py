37689e6f5112f4f082a73a0b29a20dc80d1bc5a3
100495820 <159173134+100495820@users.noreply.github.com>
2026-03-05 21:18:55 +0100

    Calculator tests added

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,18 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from calculator import add
 
-def test_add():
-    assert add(2,3) == 5
+
+def test_add_positive():
+    assert add(2, 3) == 5
+
+
+def test_add_negative():
+    assert add(-2, -3) == -5
+
+
+def test_add_zero():
+    assert add(5, 0) == 5
